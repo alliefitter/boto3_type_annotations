a26dbe95dba4e8aa8487c6c885648c4a083ddebd
Allie Fitter <afitter@cellcontrol.com>
2018-11-15 18:17:32 -0600

    Updated version.

--- boto3_type_annotations_with_docs/setup.py
+++ boto3_type_annotations_with_docs/setup.py
@@ -5,7 +5,7 @@ with open('../README.md', 'r') as f:
 
 setup(
     name='boto3_type_annotations_with_docs',
-    version='0.2.1',
+    version='0.2.2',
     packages=find_packages(),
     url='https://github.com/alliefitter/boto3_type_annotations',
     license='MIT License',
